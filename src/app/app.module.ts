b262ff299016a21c9e263536596bbaf55d587b09
Paweł <pawel@MacBook-Pro-Pawe.local>
2023-06-01 18:37:45 +0200

    update cd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
-import { EditShoppingListComponent } from './header/shopping-list/edit-shopping-list/edit-shopping-list.component';
-import { RecipeBookComponent } from './header/recipe-book/recipe-book.component';
-import { RecipeListComponent } from './header/recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './header/recipe-book/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './header/recipe-book/recipe-detail/recipe-detail.component';
-import { RecipesComponent } from './recipes/recipes.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    EditShoppingListComponent,
-    RecipeBookComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    RecipesComponent,
+
 
   ],
   imports: [
