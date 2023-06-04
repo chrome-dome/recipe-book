562a98d77a5a913fe1056c6ab3741556248a7d25
Paweł Sabuk <chrome-domepl@protonmail.com>
2023-06-04 14:49:53 +0200

    ingredients model seting up

--- src/app/shopping-list/shopping-list.model.ts
+++
@@ -1,9 +0,0 @@
-export class ShoppingList {
-    public name: string;
-    public description: string;
-
-    constructor(name: string, desc: string) {
-        this.name = name;
-        this.description = desc
-    }
-}
