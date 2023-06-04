3748467b0c493b0fe04fc0d35836a395ed722ae1
Paweł <pawel@MacBook-Pro-Pawe.local>
2023-06-04 13:10:50 +0200

    aray test

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,8 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipe: Recipe[] = [
-    new Recipe("Testing recipe", "this is a just test", "https://www.google.pl/imgres?imgurl=https%3A%2F%2Fimages.immediate.co.uk%2Fproduction%2Fvolatile%2Fsites%2F30%2F2013%2F05%2FChicken-leek-and-broccoli-rice-stir-fry-ffe0df6.jpg%3Fquality%3D90%26resize%3D556%2C505&tbnid=dwk-utopmoYIHM&vet=12ahUKEwi255qa-aT_AhXLg_0HHQNLB0EQMygJegUIARD2AQ..i&imgrefurl=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Fquick-and-healthy-recipes&docid=yEmFFlBCuS-lMM&w=556&h=505&q=recipe&ved=2ahUKEwi255qa-aT_AhXLg_0HHQNLB0EQMygJegUIARD2AQ"),
+  recipes: Recipe[] = [
+    new Recipe("Testing recipe", "this is a just test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg"),
   ];
 
   constructor() { }
