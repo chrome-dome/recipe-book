c00aaed3afc33984b784354981e427a4cc620526
Paweł Sabuk <chrome-domepl@protonmail.com>
2023-06-25 18:54:01 +0200

    display finished

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,8 +10,8 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe("Testing recipe", "this is a just test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg"),
-    new Recipe("Testing recipe", "this is a just test", "https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg"),
+    new Recipe('Testing recipe', 'this is a just test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Another Testing recipe', 'this is a just test but with another description', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
   ];
 
   constructor() { }
